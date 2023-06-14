1cbb435c71d09f7db2234df4bee1dc1212f20194
ana <132502906+anajjhu@users.noreply.github.com>
2023-06-14 14:42:57 -0300

    Matéria Representante

---
+++ materias_representante/materiasrepres.js
@@ -0,0 +1,35 @@
+function mat() {
+    window.location.href = "../lista_de_tarefas_representante/matematica/listaRepresMat.html"
+}
+
+function art() {
+    window.location.href = "../lista_de_tarefas_representante/artes/listaRepresArte.html"
+}
+
+function geo() {
+    window.location.href = "../lista_de_tarefas_representante/geografia/listaRepresGeo.html"
+}
+
+function hist() {
+    window.location.href = "../lista_de_tarefas_representante/historia/listaRepresHisto.html"
+}
+
+function ing() {
+    window.location.href = "../lista_de_tarefas_representante/ingles/listaRepresIngles.html"
+}
+
+function port() {
+    window.location.href = "../lista_de_tarefas_representante/portugues/listaRepresPort.html"
+}
+
+function quim() {
+    window.location.href = "../lista_de_tarefas_representante/quimica/listaRepresQuimica.html"
+}
+
+function edf() {
+    window.location.href = "../lista_de_tarefas_representante/ed-fisica/listaRepresEdF.html"
+}
+
+function bio() {
+    window.location.href = "../lista_de_tarefas_representante/biologia/listaRepresBiologia.html"
+}
