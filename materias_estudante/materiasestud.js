e2150dba6c0310fd58509bdab453caf2908c5bc5
ana <132502906+anajjhu@users.noreply.github.com>
2023-06-14 14:38:04 -0300

    Matéria Estudante

---
+++ materias_estudante/materiasestud.js
@@ -0,0 +1,35 @@
+function mat() {
+    window.location.href = "../lista_de_tarefas_aluno/matematica/listaAlunoMat.html"
+}
+
+function art() {
+    window.location.href = "../lista_de_tarefas_aluno/artes/listaAlunoArte.html"
+}
+
+function geo() {
+    window.location.href = "../lista_de_tarefas_aluno/geografia/listaAlunoGeo.html"
+}
+
+function hist() {
+    window.location.href = "../lista_de_tarefas_aluno/historia/listaAlunoHisto.html"
+}
+
+function ing() {
+    window.location.href = "../lista_de_tarefas_aluno/ingles/listaAlunoIngles.html"
+}
+
+function port() {
+    window.location.href = "../lista_de_tarefas_aluno/portugues/listaAlunoPort.html"
+}
+
+function quim() {
+    window.location.href = "../lista_de_tarefas_aluno/quimica/listaAlunoQuimica.html"
+}
+
+function edf() {
+    window.location.href = "../lista_de_tarefas_aluno/ed-fisica/listaAlunoEdF.html"
+}
+
+function bio() {
+    window.location.href = "../lista_de_tarefas_aluno/biologia/listaAlunoBiologia.html"
+}
