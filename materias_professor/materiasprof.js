879654973d136bb7d3247e8c7dd9a191e4e8c79f
ana <132502906+anajjhu@users.noreply.github.com>
2023-06-14 14:39:55 -0300

    Matéria Professor

---
+++ materias_professor/materiasprof.js
@@ -0,0 +1,35 @@
+function mat() {
+    window.location.href = "../lista_de_tarefas_professor/matematica/listaprofmat.html"
+}
+
+function art() {
+    window.location.href = "../lista_de_tarefas_professor/artes/listaprofarte.html"
+}
+
+function geo() {
+    window.location.href = "../lista_de_tarefas_professor/geografia/listaprofgeo.html"
+}
+
+function hist() {
+    window.location.href = "../lista_de_tarefas_professor/historia/listaprofhisto.html"
+}
+
+function ing() {
+    window.location.href = "../lista_de_tarefas_professor/ingles/listaprofingles.html"
+}
+
+function port() {
+    window.location.href = "../lista_de_tarefas_professor/portugues/listaprofport.html"
+}
+
+function quim() {
+    window.location.href = "../lista_de_tarefas_professor/quimica/listaprofquimica.html"
+}
+
+function edf() {
+    window.location.href = "../lista_de_tarefas_professor/ed-fisica/listaprofedf.html"
+}
+
+function bio() {
+    window.location.href = "../lista_de_tarefas_professor/biologia/listaprofbiologia.html"
+}
